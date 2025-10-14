6d454d5892137b8c6fb8a744c3fa9c78903a2e29
as324279 <as324275@naver.com>
2025-10-14 17:28:40 +0900

    처음커밋입니다.

---
+++ app/_layout.js
@@ -0,0 +1,18 @@
+import { Stack } from 'expo-router';
+
+export default function AuthLayout() {
+  return (
+    <Stack>
+      
+      <Stack.Screen 
+        name="index" // FirstScreen 파일명(index.js)에 해당
+        options={{
+          headerShown: false, // 헤더 숨기기 (이미지처럼 깨끗한 화면)
+          title: 'Welcome'
+        }}
+      />
+      
+      
+    </Stack>
+  );
+}
---
+++ app/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet, Pressable, ImageBackground } from 'react-native';
+import { useRouter } from 'expo-router';
+
+const FirstScreen =  () => {
+    const router = useRouter();
+
+
+    return (
+
+        <View style = {styles.container}>
+            <Image
+                source = {require('../assets/images/projectlogo.png')}
+                style = {styles.logo}
+                resizMode = "contain"
+            />
+            
+
+            <Pressable style = { [styles.Button,styles.loginButton] }>
+                <Text style = {styles.ButtonText}>로그인</Text>
+            </Pressable>
+            
+
+            <Pressable style  = { [styles.Button,styles.signButton] }>
+                <Text style = {styles.ButtonText}>회원가입</Text>
+            </Pressable>
+
+
+
+        </View>
+
+    
+
+    )
+
+    
+
+}
+export default FirstScreen;
+
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        backgroundColor: 'white', // 배경색
+        alignItems: 'center', // 가로 중앙 정렬
+        paddingTop: 80, // 상단 여백
+        paddingHorizontal: 20,
+    },
+    Button: {
+        width: '100%', // 너비 최대화
+        height: 50,
+        borderRadius: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginVertical: 8,
+        marginTop:30,    
+    },
+    ButtonText:{color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',},
+    loginButton: {backgroundColor: '#9CCC65',},
+    signButton: {backgroundColor: '#5DADE2',},
+    logo: {width: 140,
+    height: 140,
+    marginBottom: 24,}
+});
+    
