eccd94cd66ba4c665c506cf91183ed06fade2c86
as324279 <166693027+as324279@users.noreply.github.com>
2025-10-20 11:51:18 +0900

    메인 홈

--- app/(tabs)/mainHome.js
+++ app/(tabs)/mainHome.js
@@ -1,52 +1,60 @@
 import { useNavigation } from '@react-navigation/native';
 import { useRouter } from 'expo-router';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import TopHeader from '../../components/TopHeader';
 
 const MainHome = ()=>{
     const router = useRouter();
     const navigation = useNavigation();
     
     return (
-        <View style = {styles.container}>
-            {/* 채움 헤더 부분*/}
-            {/* <View style = {styles.header}>
-                <View style = {styles.headerContainer}></View>
-                <Text style = {styles.headerText}>채움</Text>
-                <Ionicons name = "notifications-outline" size = {20} color = "black" style = {styles.icon}/>
-            </View>  */}
+        <SafeAreaView  edges={['top']}>
+            <TopHeader 
+            title="채움" 
+            showBack={false}
+            showIcons={true}
+        />
+            
 
             <Pressable style = { [styles.Button,styles.groupButton] } >
                 <Text style = {styles.ButtonText}>새로운 그룹 생성</Text>
             </Pressable>
                         
             
-            <Pressable style  = { [styles.Button,styles.codeButton]} >
+            <Pressable style  = { [styles.Button1,styles.codeButton]} >
                 <Text style = {styles.ButtonText}>초대코드로 입장하기</Text>
+            </Pressable>
+
+            <Pressable style  = { [styles.Button2,styles.codeButton]} onPress={() => router.push('./ReceiptOCR')}>
+                <Text style={styles.ButtonText}>영수증 인식</Text>
             </Pressable>
             
         
             
-        </View>
+        </SafeAreaView>
     )
 }
 export default MainHome;
 
 const styles = StyleSheet.create({
-    container:{flex: 1,
+    SafeAreaView:{flex: 1,
         backgroundColor: 'white', 
         alignItems: 'center', 
         paddingTop: 80, 
         paddingHorizontal: 20,},
+    
     header : {
-        height: 80,
+    height: 80,
     backgroundColor: '#5DADE2',
     alignItems: 'center',
     justifyContent: 'center',
     },
 
-    headerContainer: {flexDirection: 'row',
-  alignItems: 'center',
-  justifyContent: 'center',},
+    headerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',},
 
     headerText: {fontSize: 22,
     fontWeight: 'bold',
@@ -57,13 +65,36 @@ const styles = StyleSheet.create({
         color: 'black'
     },
     Button: {
-        width: '100%', 
+        width: '70%', 
         height: 50,
         borderRadius: 8,
         justifyContent: 'center',
         alignItems: 'center',
         marginVertical: 8,
-        marginTop:30,    
+        marginTop:150,    
+        marginHorizontal:50
+    },
+
+    Button1: {
+        width: '70%', 
+        height: 50,
+        borderRadius: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginVertical: 8,
+        marginTop:20,    
+        marginHorizontal:50
+    },
+
+    Button2 :{
+        width: '70%', 
+        height: 50,
+        borderRadius: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginVertical: 8,
+        marginTop:20,    
+        marginHorizontal:50
     },
 
     ButtonText:{color: 'white',
